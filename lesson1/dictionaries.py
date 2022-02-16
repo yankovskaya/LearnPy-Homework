033e1d97094f4553977eb660db410a8877f035c5
«yankovskaya» <yankovskaya.my@gmail.com>
2022-02-16 19:30:59 +0300

    add changes

--- lesson1/dictionaries.py
+++ lesson1/dictionaries.py
@@ -2,9 +2,8 @@
 
 dictionary = {"city": "Москва", "temperature": "20"}
 print(dictionary["city"])
-dictionary["temperature"] = int(dictionary["temperature"])
-dictionary["temperature"] = dictionary["temperature"] - 5
-dictionary["temperature"] = str(dictionary["temperature"])
+temp = int(dictionary["temperature"]) - 5
+dictionary["temperature"] = str(temp)
 print(dictionary)
 
 # Задание 2
