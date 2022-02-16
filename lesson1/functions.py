ed2164984cede1ebb2be85ad734fda4512abe49e
«yankovskaya» <yankovskaya.my@gmail.com>
2022-02-16 19:33:12 +0300

    add changes

--- lesson1/functions.py
+++ lesson1/functions.py
@@ -1,8 +1,6 @@
 # Функции: задание 1
 
 def get_sum(one, two, delimiter='&'):
-    one = str(one)
-    two = str(two)
     return f'{one} {delimiter} {two}'
 
 result = get_sum('Learn', 'python').upper()
