5a3905357aba77071985cfcd709ebac394c8ad1d
yankovskaya <98455031+yankovskaya@users.noreply.github.com>
2022-02-16 17:21:11 +0300

    Add changes

--- lesson1/dictionaries.py
+++ lesson1/dictionaries.py
@@ -1,14 +1,16 @@
 # Задание 1
 
-dictionary = [{"city": "Москва", "temperature": "20"}]
-print(dictionary[0]["city"])
-dictionary[0]["temperature"] = "15"
+dictionary = {"city": "Москва", "temperature": "20"}
+print(dictionary["city"])
+dictionary["temperature"] = int(dictionary["temperature"])
+dictionary["temperature"] = dictionary["temperature"] - 5
+dictionary["temperature"] = str(dictionary["temperature"])
 print(dictionary)
 
 # Задание 2
 
 dictionary = {"city": "Москва", "temperature": "20"}
-print(dictionary.get("country"))
+print('country' in dictionary)
 print(dictionary.get("country", "Россия"))
 dictionary["date"]= "27.05.2019"
 print(len(dictionary))
