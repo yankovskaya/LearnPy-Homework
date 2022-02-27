d459e2748749b3f817ee9c89080b7abf2b16b13c
«yankovskaya» <yankovskaya.my@gmail.com>
2022-02-27 18:59:13 +0300

    Add changes

--- updated bot.py
+++ updated bot.py
@@ -2,8 +2,10 @@ import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import settings
 import ephem
+from datetime import datetime
 
-current_date = "2022/02/21" # Хотела сделать через datetime, но не получилось()
+dt_now = datetime.now()
+current_date = dt_now.strftime('%d/%m/%Y')
 
 planet_dict = {'Mars': ephem.Mars(current_date), 'Venus': ephem.Venus(current_date), 'Saturn': ephem.Saturn(current_date), 'Jupiter': ephem.Jupiter(current_date),
                'Neptune': ephem.Neptune(current_date), 'Uranus': ephem.Uranus(current_date), 'Mercury': ephem.Mercury(current_date)}
@@ -30,12 +32,12 @@ def name_planet(update, context):
     print('Вызван /planet')
     user_txt = update.message.text.split()[1]
     user_planet = planet_dict.get(user_txt, None)
-    if user_planet != None:
-        constellation = ephem.constellation(planet_dict[user_txt])
-        update.message.reply_text(f'Constellathion: {constellation[1]}')
-    else: 
+    if not user_planet:
         update.message.reply_text("Whaaat?")
-        
+    if user_planet == True:
+        constellation = ephem.constellation(user_planet)
+        update.message.reply_text(f'Constellathion: {constellation[1]}')
+
 
 
 def main():
